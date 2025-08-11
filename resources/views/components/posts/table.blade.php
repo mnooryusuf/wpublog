5a35ba4f6f267a570d7edee8cd6f3ae8367c1f01
Muhammad Noor Yusuf <mnyusuf14@gmail.com>
2025-08-11 15:50:38 +0800

    Post Dashboard Search & Pagination

--- resources/views/components/posts/table.blade.php
+++ resources/views/components/posts/table.blade.php
@@ -1,10 +1,12 @@
 <section class=" p-3 sm:p-5 antialiased">
+    <h2 class="text-xl font-semibold pl-4 text-gray-900 dark:text-white mb-4">
+        Posts by: {{ Auth::user()->name }}</h2>
     <div class=" max-w-screen-xl px-4 ">
         <div class="bg-white dark:bg-gray-800 relative border sm:rounded-lg overflow-hidden">
             <div class="flex flex-col md:flex-row items-center justify-start space-y-3 md:space-y-0 md:space-x-4 p-4">
                 <div class="w-full md:w-1/2">
-                    <form class="flex items-center">
-                        <label for="simple-search" class="sr-only">Search</label>
+                    <form class="flex items-center" action="" method="GET">
+                        <label for="simple-search" class="sr-only">Search Post</label>
                         <div class="relative w-full">
                             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                 <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
@@ -14,9 +16,9 @@
                                         clip-rule="evenodd" />
                                 </svg>
                             </div>
-                            <input type="text" id="simple-search"
+                            <input type="text" id="simple-search" name="keyword"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                placeholder="Search" required="">
+                                placeholder="Search Posts">
                         </div>
                     </form>
                 </div>
@@ -41,7 +43,6 @@
                         <tr>
                             <th scope="col" class="px-4 py-4">#</th>
                             <th scope="col" class="px-4 py-3">Title</th>
-                            <th scope="col" class="px-4 py-3">Author</th>
                             <th scope="col" class="px-4 py-3">Category</th>
                             <th scope="col" class="px-4 py-3">Published At</th>
                             <th scope="col" class="px-4 py-3">
@@ -56,7 +57,6 @@
                                     class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     {{ $loop->iteration }}</th>
                                 <td class="px-4 py-3">{{ $post->title }}</td>
-                                <td class="px-4 py-3">{{ $post->author->name }}</td>
                                 <td class="px-4 py-3 max-w-[12rem] truncate">{{ $post->category->name }}</td>
                                 <td class="px-4 py-3">{{ $post->created_at->diffForHumans() }}</td>
                                 <td class="px-4 py-3 flex items-center justify-end">
@@ -124,9 +124,11 @@
                     </tbody>
                 </table>
             </div>
-            <div class="p-4">
-                {{ $posts->links() }}
-            </div>
+            @if ($posts->hasPages())
+                <div class="p-4">
+                    {{ $posts->links() }}
+                </div>
+            @endif
         </div>
 </section>
 <!-- End block -->
